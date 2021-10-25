10959f6b93def457b10fc5068719155b34e83d71
Moki <mokiong1427@gmail.com>
2021-10-25 19:26:49 +0800

    Add handlers for context

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,13 +1,29 @@
 import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
     notification: null,
-    showNotification: function () {},
+    showNotification: function (notificationData) {},
     hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+    const [activeNotification, setActiveNotification] = useState();
+
+    function showNotificationHandler(notificationData) {
+        setActiveNotification(notificationData);
+    }
+
+    function hideNotificationHandler() {
+        setActiveNotification(null);
+    }
+
+    const context = {
+        notification: activeNotification,
+        showNotification: showNotificationHandler,
+        hideNotification: hideNotificationHandler,
+    };
+
     return (
         <NotificationContext.Provider>
             {props.children}
